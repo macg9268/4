b090c2249fa4c03af7871172233ee6252d2ebbba
Mauricio Capiz <mauricio.capiz@gmail.com>
2025-12-30 05:53:38 +0000

    build and add logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,20 +4,20 @@ export function Logo({ className }: { className?: string }) {
   return (
     <svg
       className={cn("w-auto h-8", className)}
-      viewBox="0 0 64 44"
+      viewBox="0 0 52 38"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       aria-label="4Cloud Logo"
     >
       <path
-        d="M48.79,21.32A16,16,0,0,0,34.2,8.19a12,12,0,0,0-23.6,5.12,10,10,0,0,0-1,19.95H47.48a12.5,12.5,0,0,0,1.31-25Z"
+        d="M37.82,18.82A13.91,13.91,0,0,0,25.4,7.12a10.5,10.5,0,0,0-20.65,4.48,8.75,8.75,0,0,0-.87,17.5H41.64a10.94,10.94,0,0,0,1.15-21.88Z"
         stroke="hsl(var(--primary))"
         strokeWidth="3"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
-        d="M36 20 L30 20 L30 26 L36 26 M30 26 L30 32"
+        d="M33 24 L39 24 M36 21 L36 27"
         stroke="hsl(var(--primary))"
         strokeWidth="3"
         strokeLinecap="round"
