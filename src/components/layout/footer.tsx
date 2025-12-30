9ca14562a646591ed8f0c82ccb4f36621a9660a1
Mauricio Capiz <mauricio.capiz@gmail.com>
2025-12-30 06:42:39 +0000

    delete footer logo

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import { Github } from 'lucide-react';
-import { Logo } from '@/components/logo';
 import { Button } from '@/components/ui/button';
 
 export function Footer() {
@@ -8,7 +7,6 @@ export function Footer() {
     <footer className="border-t bg-secondary">
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <Logo className="h-8 w-auto" />
           <p className="text-center text-sm leading-loose md:text-left">
             A modern cloud solutions provider.
           </p>
